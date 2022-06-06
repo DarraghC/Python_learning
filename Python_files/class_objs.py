fa946552605802b3d560415caf9eeff8b2a8d671
Darraghc <darragh-clerkin@hotmail.com>
2022-06-07 00:10:52 +0100

    Snakes and Ladders

--- Python_files/class_objs.py
+++ Python_files/class_objs.py
@@ -1,13 +1,13 @@
 from dataclasses import dataclass
+from random import randint
 
-
-@dataclass
+# @dataclass
 class Car:
-    make: str
-    model: str
-    # def __init__(self, make, model):
-    #     self.make = make
-    #     self.model = model
+    # make: str
+    # model: str
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
 
     def first_function(self):
         print("My car is a {0} {1}".format(self.make, self.model))
@@ -27,4 +27,36 @@ class NewCar(Car):
 # MyNewCar = NewCar("Scoda", "Octavia")
 # MyNewCar.first_function()
 
+
+class Player:
+
+    def __init__(self, name):
+        self.name = name
+        self.position = 0
+        self.role = 0
+        self.ladders_dict = {1: 38, 4: 14, 8: 30, 21: 42, 28: 76, 50: 67, 71: 92, 80: 99}
+        self.snakes_dict = {32: 10, 36: 6, 48: 26, 62: 18, 88: 24, 95: 56, 97: 78}
+
+    def role_dice(self):
+        if self.position < 100:
+            role = randint(0, 6)
+            self.position += role
+
+    def check_for_snakes(self):
+        # print("Checking for Snakes")
+        for start_pos, end_pos in self.snakes_dict.items():
+            if start_pos == self.position:
+                self.position = end_pos
+                print("Snakes!!!!")
+
+    def check_for_ladders(self):
+        # print("Checking for Ladders")
+        for start_pos, end_pos in self.ladders_dict.items():
+            if start_pos == self.position:
+                self.position = end_pos
+                print("Ladders!!!!!!")
+
+    def increment_role(self):
+        self.role += 1
+        # print("{0} is on role {1}".format(self.name, self.role))
 
--- Python_files/main_file.py
+++ Python_files/main_file.py
@@ -1,12 +1,36 @@
-from class_objs import Car, NewCar
+from class_objs import Car, NewCar, Player
 
 
 def main():
-    MyCar = Car("Seat", "Leon")
-    MyCar.first_function()
-    print(MyCar)
-    MyNewCar = NewCar("Scoda", "Octavia")
-    MyNewCar.first_function()
+
+    Player1 = Player("Player 1")
+    Player2 = Player("Player 2")
+    while Player1.position < 100 or Player2.position < 100:
+        # print("Player 1 start position is at {0}".format(Player1.position))
+        Player1.role_dice()
+        Player1.check_for_ladders()
+        Player1.check_for_snakes()
+        Player1.increment_role()
+        # print("Player 1 end position is at {0}".format(Player1.position))
+        if Player1.position >= 100:
+            print("Player 1 Wins on role {0}".format(Player1.role))
+            break
+
+        # print("Player 2 start position is at {0}".format(Player2.position))
+        Player2.role_dice()
+        Player2.check_for_ladders()
+        Player2.check_for_snakes()
+        Player2.increment_role()
+        # print("Player 2 end position is at {0}".format(Player2.position))
+        if Player2.position >= 100:
+            print("Player 2 Wins on role {0}".format(Player2.role))
+            break
+
+    # MyCar = Car("Seat", "Leon")
+    # MyCar.first_function()
+    # print(MyCar)
+    # MyNewCar = NewCar("Scoda", "Octavia")
+    # MyNewCar.first_function()
 
 
 if __name__ == "__main__":
